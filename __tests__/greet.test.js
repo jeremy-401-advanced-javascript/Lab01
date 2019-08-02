f356c1d4c7f511a9f9bbc9eb12b639d81d768788
Jeremy Lee <www.jeremy.llee27@gmail.com>
2019-08-01 22:05:44 -0700

    Pushing for final submission

--- __tests__/greet.test.js
+++ __tests__/greet.test.js
@@ -1,10 +1,14 @@
 'use strict';
 
 const greet = require('../lib/greet.js');
+const faker = require('faker');
 
 describe('greet module', () => {
   it('greets a person', () => {
-    expect(true).toBeTruthy();
+    let myWord = faker.random.number();
+    let expected = `Hi, ${myWord}`;
+    // let greeting = greet.sayHello(myWord);
+    expect(greet.sayHello(myWord)).toEqual(expected);
   });
 });
 
